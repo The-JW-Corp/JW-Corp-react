0a98b2922281f98def06e71d901239c98f400e25
Ra <106039941+JW-Rami@users.noreply.github.com>
2022-10-25 17:27:35 +0200

    Arrow icon added + responsive for 500 width

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -8,12 +8,12 @@ function LandingPage() {
       <div>
         <h1 className="titre-landing-page">
           <div className="first-text-landing-page">
-            Accompagner
+            Accompagner 
             <div className="line-and-word">
               <span className="underline-landing-page">aujourd'hui</span>
               <span className="liner"></span>
             </div>
-            pour la décentralisation de 
+            pour la décentralisation de  
             <div className="line-and-word2">
               <span className="underline-landing-page2">demain.</span>
               <span className="liner transition-liner"></span>
@@ -40,7 +40,16 @@ function LandingPage() {
             </div>
           </div>
         </h1>
-
+        <div className="arrow-bloc-landing-page bouncing-animation">
+          <svg
+            className="arrow-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+          >
+            {/*! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
+            <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+          </svg>
+        </div>
       </div>
     </section>
   );
