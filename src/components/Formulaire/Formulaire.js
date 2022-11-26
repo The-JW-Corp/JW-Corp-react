931bab8617abb8af838ad41d7bb68d1f039e7939
JW Rami <rami@jwcorp.fr>
2022-11-26 17:17:19 +0200

    text and form added

--- src/components/Formulaire/Formulaire.js
+++ src/components/Formulaire/Formulaire.js
@@ -3,64 +3,65 @@ import "./Formulaire.css";
 
 function Formulaire() {
   return (
-    <div className="formulaire">
-      <section id="" className="levraitextavantform">
-        <div id="" className="jsAnimation containerOfcontaintainerSize">
-          <div id="" className="containerOftextavantform">
-            <span id="textavantform">
-              Vous souhaitez prendre contact ? Vous avez un projet Blockchain,
-              un besoin d'accompagnement, de développement ? Un retour à nous
-              faire ? <br />
-              <br /> Laissez-nous votre message nous reviendrons vers vous au
-              plus vite !
-              <br />
-              <br />
-              <br />
-            </span>
+    <section className="container-formulaire">
+      <div className="formulaire">
+        <section id="" className="levraitextavantform">
+          <div id="" className="jsAnimation containerOfcontaintainerSize">
+            <div id="" className="containerOftextavantform">
+              <span id="textavantform">
+                Vous souhaitez prendre contact ? Vous avez un projet Blockchain,
+                un besoin d'accompagnement, de développement ? Un retour à nous
+                faire ? <br />
+                <br /> Laissez-nous votre message nous reviendrons vers vous au
+                plus vite !
+                <br />
+                <br />
+                <br />
+              </span>
+            </div>
           </div>
-        </div>
-      </section>
-      <div className="spaces" />
-      <section className="texteuh">
-        <div className="deuxieme">
-          {/* <span> */}
-          <div className="login-box">
-            <h2 id="formulairedecontact">CONTACT</h2>
-            <form method="post">
-              <div className="user-box">
-                <input
-                  id="leEmail"
-                  type="text"
-                  name="Email"
-                  required=""
-                  placeholder=""
-                />
-                <label htmlFor="leEmail">Email</label>
-              </div>
+        </section>
 
-              <div className="user-box">
-                <textarea
-                  id="votremessage"
-                  name="Message"
-                  type="text"
-                  placeholder=""
-                  required=""
-                  defaultValue={""}
-                />
-                <label htmlFor="votremessage">Votre message</label>
-              </div>
-              <button type="text" name="envoyer" id="police-submit">
-                <span />
-                <span />
-                <span />
-                <span />
-                Envoyer
-              </button>
-            </form>
+        <section className="texteuh">
+          <div className="deuxieme">
+            <div className="login-box">
+              <h2 id="formulairedecontact">CONTACT</h2>
+              <form method="post">
+                <div className="user-box">
+                  <input
+                    id="leEmail"
+                    type="text"
+                    name="Email"
+                    required=""
+                    placeholder=""
+                  />
+                  <label htmlFor="leEmail">Email</label>
+                </div>
+
+                <div className="user-box">
+                  <textarea
+                    id="votremessage"
+                    name="Message"
+                    type="text"
+                    placeholder=""
+                    required=""
+                    defaultValue={""}
+                  />
+                  <label htmlFor="votremessage">Votre message</label>
+                </div>
+                <button type="text" name="envoyer" id="police-submit">
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                  Envoyer
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </section>
   );
 }
 
