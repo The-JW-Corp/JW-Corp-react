fde656a6a18d27677c2544388a923da5980fe070
JW Rami <rami@jwcorp.fr>
2022-12-12 01:09:54 +0200

    form design from scratch

--- src/components/Formulaire/Formulaire.js
+++ src/components/Formulaire/Formulaire.js
@@ -1,10 +1,51 @@
 import React from "react";
+import { useState } from "react";
 // import "./Formulaire.css";
 import "./TestForm.css";
 
 function Formulaire() {
-  return <section>
-    
-  </section>;
+  // const [name, setName] = useState("");
+
+  return (
+    <section className="form-container">
+      {/* <form>
+        <label>
+          Email
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Votre nom
+          <textarea name="nom" id=""
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          ></textarea>
+        </label>
+
+        <input type="submit" value="Envoyer" />
+      </form> */}
+      <form>
+        <div className="mail-container">
+          <label className="labels">
+            <div className="text-label">
+            Email
+            </div>
+            <div className="mail-input">
+              <input type="text" name="email" />
+            </div>
+          </label>
+        </div>
+        <div className="message-container">
+          <label className="labels">
+            <div className="text-label">
+            Votre message
+            </div>
+            <div className="message-input">
+              <textarea name="message"></textarea>
+            </div>
+          </label>
+        </div>
+      </form>
+    </section>
+  );
 }
 export default Formulaire;
