06322bdc2b64cd6ddbbd41b1273f119a864d5a73
JW Matheo <matmatvallone8@gmail.com>
2022-12-12 21:02:24 +0200

    set up Services

--- src/page/Services/Services.js
+++ src/page/Services/Services.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Services.css';
 import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 const Services = () => {
   return (
-    <div>
-      <Link to="/#test">Heelo</Link>
-    </div>
+    <>
+      <Navbar />
+      <Footer />
+    </>
+    // <div>
+    //   <Link to="/#test">Heelo</Link>
+    // </div>
   )
 }
 
