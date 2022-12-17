c4e9b8f543a3dbb5e649ecdfe9ecce2837a191ce
JW Rami <rami@jwcorp.fr>
2022-12-17 05:37:27 +0200

    firebase dependancy added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Home from "./components/Home/Home";
+import { firebaseConfig } from "./configDb";
+import { FirebaseDatabaseProvider} from "@react-firebase/database";
+import firebase from "firebase";
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+// import 'firebase/compat/storage'
 function App() {
+  console.log(firebaseConfig);
   return (
-    <div>
-      <Home />
-    </div>
+    <FirebaseDatabaseProvider {...firebaseConfig} firebase={firebase}>
+      <div>
+        <Home />
+      </div>
+    </FirebaseDatabaseProvider>
   );
 }
 
